f19c0173e449e29bdaaaff7020b9fa87871449db
NYA Zakaria <zakaria.nya@hetic.net>
2018-10-27 12:54:56 +0200

    ajoute le mouvement de la balle dans le canvas, sans quelle sorte de celui-ci, ajoute le rebondissement, et ajoute les commentaires

--- javascript/main.js
+++ javascript/main.js
@@ -1,25 +1,36 @@
 
-
+// crée le canvas et lui donne les dimensions
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
+// nos variables 
 var x = canvas.width/5;
 var y = canvas.height-40;
-var dx = 10;
-var dy = -4;
+var dx = 2;
+var dy = -2;
+var ballRadius = 10;
 
+// nos fonctions pour la balle et le rectangle
 function drawBall(){
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
 }
 
+
+// function qui donne le mouvement à une balle 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
     x += dx;
     y += dy;
+    if(y + dy > canvas.height - ballRadius || y + dy < ballRadius){
+        dy = -dy;  
+    }
+    if( x + dx > canvas.width - ballRadius || x + dx < ballRadius){
+        dx = -dx;   
+    }
 }
 setInterval(draw, 10);
