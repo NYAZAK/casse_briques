443e1e364c8e0156ea047ba5d6a9a9a56ce05ac1
NYA Zakaria <zakaria.nya@hetic.net>
2018-10-27 12:06:40 +0200

    ajoute des fonctions pour le dessin de la balle, et une fonction pour le mouvement de la balle

--- javascript/main.js
+++ javascript/main.js
@@ -3,10 +3,23 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+var x = canvas.width/5;
+var y = canvas.height-40;
+var dx = 10;
+var dy = -4;
 
+function drawBall(){
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
 
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    x += dx;
+    y += dy;
+}
+setInterval(draw, 10);
